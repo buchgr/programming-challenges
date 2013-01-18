8a05cb61d174419c2aee0f7ef36be85b0537e88f
Jakob Buchgraber <bucjac@gmail.com>
2013-01-18 16:39:30 +0100

    cleaned up code in bestbefore

--- bestbefore.py
+++ bestbefore.py
@@ -4,9 +4,9 @@ import re
 def valid_date(year, month, day):
     days_a_month = (31,29,31,30,31,30,31,31,30,31,30,31)
     
-    if month > 12:
+    if month < 1 or month > 12:
         return False
-    if day > days_a_month[month-1]:
+    if day < 1 or day > days_a_month[month-1]:
         return False
     if month == 2 and day == 29 and not is_leap_year(year):
         return False
@@ -21,10 +21,11 @@ def is_leap_year(year):
 def min_date(parts):
     # there are 3*2*1 = 6 possible ways to order the date parts
     date_map = [(0,1,2),(0,2,1),(1,0,2),(1,2,0),(2,0,1),(2,1,0)]
-    
+    parts.sort()
+
     for d in date_map:
         year, month, day = parts[d[0]], parts[d[1]], parts[d[2]]
-        if year < 1000: year += 2000
+        if year < 2000: year += 2000
         if valid_date(year, month, day):
             return "{0}-{1:02d}-{2:02d}".format(year, month, day)
 
@@ -34,18 +35,11 @@ if __name__ == "__main__":
     raw_date = raw_input();
     
     # if not re.match("(\d|\d{2}|\d{4})/(\d|\d{2}|(?<!\d{4}/)\d{4})/(\d|\d{2}|(?<!\d{4}/\2/)\d{4})$", raw_date):
-    date_parts = sorted([int(n) for n in raw_date.split('/')])
-    illegal_input = False
-    for p in date_parts:
-        if p < 0 or p >= 3000 or len(str(p)) == 3:
-            illegal_input = True
-            sys.stdout.write(raw_date + "is illegal")
-            break
+    date_parts = [int(n) for n in raw_date.split('/')]
+    date = min_date(date_parts)
 
-    if not illegal_input:
-        date = min_date(date_parts)
-        if date is not None:
-            # don't print any additional spaces or newlines
-            sys.stdout.write(date)
-        else:
-            sys.stdout.write(raw_date + " is illegal")
+    if date is not None:
+        # don't print any additional spaces or newlines
+        sys.stdout.write(date)
+    else:
+        sys.stdout.write(raw_date + " is illegal")
